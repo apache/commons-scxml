343dbfe671f8c486e3b292ee879a9dc89631f4c5
Gary Gregory <garydgregory@gmail.com>
2023-03-19 16:26:06 -0400

    Javadoc: Convert package.html to package-info.java

--- src/main/java/org/apache/commons/scxml2/env/package-info.java
+++ src/main/java/org/apache/commons/scxml2/env/package-info.java
@@ -1,5 +1,4 @@
-<html>
-<!-- 
+/*
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
@@ -14,16 +13,13 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
--->
-<head>
-</head>
-<body>
+ */
 
-  <p>A collection of classes that may be commonly used to bridge the
-     SCXML executor to the runtime environment.</p>
-     
-  <p>Various sub-packages contain specifics related to a particular
-     environment.</p>
-
-</body>
-</html>
+/**
+ * A collection of classes that may be commonly used to bridge the SCXML executor to the runtime environment.
+ *
+ * <p>
+ * Various sub-packages contain specifics related to a particular environment.
+ * </p>
+ */
+package org.apache.commons.scxml2.env;
