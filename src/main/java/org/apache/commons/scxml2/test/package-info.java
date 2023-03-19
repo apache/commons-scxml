fb8aff42cd67fd7f281fc33008ab0619ff7506f9
Gary Gregory <garydgregory@gmail.com>
2023-03-19 16:26:34 -0400

    Javadoc: Convert package.html to package-info.java

--- src/main/java/org/apache/commons/scxml2/test/package-info.java
+++ src/main/java/org/apache/commons/scxml2/test/package-info.java
@@ -1,5 +1,4 @@
-<html>
-<!-- 
+/*
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
@@ -14,12 +13,9 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
--->
-<head>
-</head>
-<body>
+ */
 
-  <p>A collection of classes useful for command line testing.</p>
-
-</body>
-</html>
+/**
+ * A collection of classes useful for command line testing.
+ */
+package org.apache.commons.scxml2.test;
