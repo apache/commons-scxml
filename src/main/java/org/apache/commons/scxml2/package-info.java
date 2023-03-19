12080d25841434aaebb7ba1218dfc2a9ac0935d7
Gary Gregory <garydgregory@gmail.com>
2023-03-19 16:26:39 -0400

    Javadoc: Convert package.html to package-info.java

--- src/main/java/org/apache/commons/scxml2/package-info.java
+++ src/main/java/org/apache/commons/scxml2/package-info.java
@@ -1,5 +1,4 @@
-<html>
-<!-- 
+/*
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
@@ -14,12 +13,9 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
--->
-<head>
-</head>
-<body>
+ */
 
-  <p>The Commons SCXML executor and core concepts.</p>
-
-</body>
-</html>
+/**
+ * The Commons SCXML executor and core concepts.
+ */
+package org.apache.commons.scxml2;
