772d60e89b393c2389e5d5bb39476bcee67f6f99
Gary Gregory <garydgregory@gmail.com>
2024-09-12 10:45:44 -0400

    Use final

--- src/main/java/org/apache/commons/scxml2/env/URLResolver.java
+++ src/main/java/org/apache/commons/scxml2/env/URLResolver.java
@@ -38,7 +38,7 @@ public class URLResolver implements PathResolver, Serializable {
     private static final Log log = LogFactory.getLog(PathResolver.class);
 
     /** The base URL to resolve against. */
-    private URL baseURL;
+    private final URL baseURL;
 
     /**
      * Constructs a new instance.
